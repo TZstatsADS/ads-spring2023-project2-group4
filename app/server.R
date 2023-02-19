874f3ce670535cb0fd1b8a1b193a71e4419404ea
Liang Hu <lh3057@columbia.edu>
2023-02-19 13:16:59 -0500

    current app

--- app/server.R
+++ app/server.R
@@ -8,36 +8,49 @@
 #
 ###############################Install Related Packages #######################
 if (!require("shiny")) {
-    install.packages("shiny")
-    library(shiny)
+  install.packages("shiny")
+  library(shiny)
 }
 if (!require("leaflet")) {
-    install.packages("leaflet")
-    library(leaflet)
+  install.packages("leaflet")
+  library(leaflet)
 }
 if (!require("leaflet.extras")) {
-    install.packages("leaflet.extras")
-    library(leaflet.extras)
+  install.packages("leaflet.extras")
+  library(leaflet.extras)
 }
 if (!require("dplyr")) {
-    install.packages("dplyr")
-    library(dplyr)
+  install.packages("dplyr")
+  library(dplyr)
 }
 if (!require("magrittr")) {
-    install.packages("magrittr")
-    library(magrittr)
+  install.packages("magrittr")
+  library(magrittr)
 }
-if (!require("mapview")) {
-    install.packages("mapview")
-    library(mapview)
-}
+
 if (!require("leafsync")) {
-    install.packages("leafsync")
-    library(leafsync)
+  install.packages("leafsync")
+  library(leafsync)
 }
 if (!require("rsconnect")) {
   install.packages("rsconnect")
   library(rsconnect)
+}
+if (!require("geojsonio")) {
+  install.packages("geojsonio")
+  library(geojsonio)
+}
+if (!require("tidyverse")) {
+  install.packages("tidyverse")
+  library(tidyverse)
+}
+if (!require("ggplot2")) {
+  install.packages("ggplot2")
+  library(ggplot2)
+}
+if (!require("tidyr")) {
+  install.packages("tidyr")
+  library(tidyr)
 }
 
 #host website
@@ -54,25 +67,164 @@ if (!require("rsconnect")) {
 
 #Data Processing
 df = read.csv('./data/Queens_Library_Branches.csv')
-df <- df[which(!is.na(df$Latitude) & !is.na(df$Longitude)),]
+#df <- df[which(!is.na(df$Latitude) & !is.na(df$Longitude)),]
 # Check for missing or invalid lat/lon values
-df <- df[, c("Latitude", "Longitude")]
+#df <- df[, c("Latitude", "Longitude")]
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-  # create a point plot with the latitude and longitude columns
-output$map <- renderLeaflet({
-    leaflet(data = df) %>% 
-    addMarkers(lng = df$Longitude, lat = df$Latitude)
+  
+  ######################. map  by days################################  
+  queensLat <- 40.742054
+  queensLon <- -73.769417
+  usaZoom <- 10.4
+  
+  
+  
+  filtered = reactive({
+    if (input$Day=="Monday"){
+      return (subset(df,grepl("Closed",df[,"Mn"])==FALSE))}
+    if (input$Day=="Tuesday"){
+      return (subset(df,grepl("Closed",df[,"Tu"])==FALSE))}
+    if (input$Day=="Wednesday"){
+      return (subset(df,grepl("Closed",df[,"We"])==FALSE))}
+    if (input$Day=="Thursday"){
+      return (subset(df,grepl("Closed",df[,"Th"])==FALSE))}
+    if (input$Day=="Friday"){
+      return (subset(df,grepl("Closed",df[,"Fr"])==FALSE))}
+    if (input$Day=="Saturday"){
+      return (subset(df,grepl("Closed",df[,"Sa"])==FALSE))}
+    if (input$Day=="Sunday"){
+      return (subset(df,grepl("Closed",df[,"Su"])==FALSE))}
+  })
+  
+  icons <- awesomeIcons(
+    icon = 'ios-close',
+    iconColor = 'white',
+    library = 'ion',
+    markerColor = "blue"
+  )
+  
+  ####### map ##############################################################
+  
+  output$map = renderLeaflet(
+    
+    leaflet() %>%
+      setView(lat = queensLat, lng = queensLon, zoom = usaZoom) %>%
+      addTiles(urlTemplate = 'http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga', attribution = 'Google') %>% 
+      addMarkers(data=filtered(), ~Longitude, ~Latitude, icon = icons,
+                 popup = paste("Name: ", df$name, "<br>",
+                               "Address: ", df$Address)))
+  
+  
+  ##################################### Map ends ###################################
+  
+  
+  
+  
+  
+  
+  
+  ##############plot###########
+  # ---------------------Search By Location output - by Shiver ------------------------
+  df2=read.csv('./data/Queens_Library_Branches.csv')
+  output$city_piechart <- renderPlot({
+    df2$City <- trimws(df2$City)
+    temp <- df2[grepl(input$City, df2$City), ]
+    data <- c(temp$Community.Board)
+    labels <- c(temp$name)
+    pie(data, labels <- labels, main = 'Library popularity by city' ) 
+  })
+  
+  output$zip_piechart <- renderPlot({
+    df2$City <- trimws(df2$City)
+    temp <- df2[substr(df2$Postcode, 1, 3) == input$Zipcode, ]
+    data <- c(temp$Community.Board)
+    labels <- c(temp$name)
+    pie(data, labels <- labels, main = 'Library popularity by zipcode' )
   })
   
   
+  # ---------------------Search By day output - by Jingshu ------------------------
+  output$distPlot <- renderPlot({
+    
+    data = read.csv("./data/Queens_Library_Branches.csv")
+    
+    data_cleaned = data[which(!is.na(data$Latitude) & !is.na(data$Longitude)),] 
+    
+    cleaned_data = data_cleaned %>%
+      rename('Monday' = 'Mn', 'Tuesday' = 'Tu', 'Wednesday' = 'We', 'Thursday' = 'Th', 'Friday' = 'Fr', 'Saturday' = 'Sa', 'Sunday' = 'Su') %>%
+      gather("Day", "Time", 6:12) %>%
+      filter(Day == input$mmm)
+    
+    ggplot(cleaned_data, aes(x = Time, fill = factor(Time))) +
+      geom_bar(stat = "count") +
+      xlab("Time Slot") +
+      ylab("Number of Available Libraries") + scale_x_discrete(guide = guide_axis(angle = 45))+
+      ggtitle("Library Availablity across Day") +
+      theme(legend.position = "none")
+  }) #renderPlot closes
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ##################
+  
+  ########### popularity map   #############
+  neighbor <- geojsonio::geojson_read("https://data.beta.nyc/dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson", what = "sp")
+  bins <- c(0,2,4,6,8,10,12, Inf)
+  pal <- colorBin("YlOrRd", domain = df$Community.Board, bins = bins)
+  labels <- sprintf("<strong>%s</strong><br/>%g Community Boards", neighbor$neighborhood, rep(df$Community.Board, length.out = length(neighbor$neighborhood)))%>%lapply(htmltools::HTML)
+  output$m = renderLeaflet(leaflet(neighbor) %>%
+                             setView(lat = queensLat, lng = queensLon, zoom = usaZoom) %>%
+                             addTiles(urlTemplate = 'http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga', attribution = 'Google') %>%
+                             addProviderTiles("MapBox", options = providerTileOptions(
+                               id = "mapbox.light",
+                               accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+                             addPolygons(
+                               fillColor = ~pal(df$Community.Board),
+                               weight = 2,
+                               opacity = 1,
+                               color = "white",
+                               dashArray = "3",
+                               fillOpacity = 0.7,
+                               highlightOptions = highlightOptions(
+                                 weight = 5,
+                                 color = "#999",
+                                 dashArray = "",
+                                 fillOpacity = 0.7,
+                                 bringToFront = TRUE),
+                               label = labels,
+                               labelOptions = labelOptions(
+                                 style = list("font-weight" = "normal", padding = "3px 8px"),
+                                 textsize = "15px",
+                                 direction = "auto")) %>% 
+                             addLegend(pal = pal, values = ~df$Community.Board, opacity = 0.7, title = "Number of Community Board",
+                                       position = "topright"))
+  
+#####################  
+  
+ 
+  
+  
+  
+  #####
+  
   ## search table output #############################################
-output$search_result = DT::renderDataTable({
+  output$search_result = DT::renderDataTable({
     read.csv("./data/Queens_Library_Branches.csv")%>%
       select(name,City,Postcode)
   },selection = 'single')
-
+  
 })
+  
 
 
--- app/ui.R
+++ app/ui.R
@@ -22,57 +22,160 @@ if (!require("leaflet.extras")) {
 
 # Define UI for application that draws a histogram
 shinyUI(
-    navbarPage(strong("LibGo",style="color: white;"), 
-               theme=shinytheme("cosmo"), # select your themes https://rstudio.github.io/shinythemes/
-               
-               tabPanel("Introduction",
-                        tags$img(
-                          src = "https://live.staticflickr.com/5151/5914733818_983804b390_b.jpg",
-                          width = "100%",
-                          style = "opacity: 0.90"
-                        ),
-                        fluidRow(
-                          absolutePanel(
-                            style = "background-color: rgba(0, 0, 0, .3)",
-                            top = "40%",
-                            left = "25%",
-                            right = "25%",
-                            height = 270,
-                            tags$p(
-                              style = "padding: 5%; background-color: rgba(0, 0, 0, .3);font-family: alegreya; font-size: 220%; color: White; text-align: center",
-                              "LibGo"
-                            ),
-                            tags$p(
-                              style = "color: White", 
-                              "a brief introduction"
-                            )
+  navbarPage(strong("LibGo",style="color: white;"), 
+             theme=shinytheme("cosmo"), # select your themes https://rstudio.github.io/shinythemes/
+             
+             tabPanel("Introduction",
+                      tags$img(
+                        src = "https://live.staticflickr.com/5151/5914733818_983804b390_b.jpg",
+                        width = "100%",
+                        style = "opacity: 0.90"
+                      ),
+                      fluidRow(
+                        absolutePanel(
+                          style = "background-color: rgba(0, 0, 0, .3)",
+                          top = "40%",
+                          left = "25%",
+                          right = "25%",
+                          height = 270,
+                          tags$p(
+                            style = "padding: 5%; background-color: rgba(0, 0, 0, .3);font-family: alegreya; font-size: 420%; color: White; text-align: center",
+                            "LibGo"
+                          ),
+                          tags$p(
+                            style = "padding: 5%; background-color: rgba(0, 0, 0, .3); color: White;font-size: 120%;", 
+                            "LibGo is an app developed based on the Queens libraries provided by NYC OpenData. Our goal is to satisfy the needs of those who need to visit the library and provide them with the information they are seeking for. Specifically, this app visualizes the location, operating time and relative popularity data in the greater Queens borough. The user can use either the interactive maps or the search panels to retrieve the required information. "
                           )
                         )
-               ), # tabPanel closing 
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         leafletOutput("map", width="100%", height="100%"),
-                        ), #tabPanel maps closing
+                      )
+             ), # tabPanel closing 
+             ##########------------------------------- tab panel - Maps By Days ---------------------------------
+             tabPanel("Map By Days",
+                      sidebarLayout(
+                        sidebarPanel(
+                          titlePanel("Look up for open libraries"),
+                          fluidRow(column(7,
+                                          selectInput("Day", label = h2("Select Day"), 
+                                                      choices = list("Monday","Tuesday","Wednesday","Thursday","Friday",
+                                                                     "Saturday","Sunday")
+                                          ))),
+                          width = 3),
+                        mainPanel(
+                          leafletOutput("map"),
+                          width = 9
+                        ),
+                        position = c("left", "right"),
+                        fluid = TRUE
+                      )
+                      
+                      
+                      
+             ), #tabPanel maps closing
+             #########################PopularityHeatMap#######          
+             
+             tabPanel("Map By Popularity",
+                      leafletOutput("m", height = "800px")
+             ),
+             
+             
+             ##########################--------------------------------------------------------------------
+#######plot#########
+# ------------------------------- tab panel - Interactive Plot by Location - Shiver ---------------------------------
+tabPanel("Interactive Plot By Location",
+         sidebarPanel(
+           selectInput(inputId = "City",
+                       label = "Choose by City:",
+                       choices = c( "Jamaica", "Flushing", "Long Island City","Elmhurst","Ozone Park","Corona",
+                                    "Hollis","Elmhurst","Rockaway"))
+           
+         ),
+         mainPanel(
+           plotOutput('city_piechart')
+         ),
+         
+         sidebarPanel(
+           selectInput(inputId = "Zipcode",
+                       label = "Choose by first 3 digit of Zipcode:",
+                       choices = c( '111','113','114','116'
+                       ))
+           
+         ),
+         mainPanel(
+           plotOutput('zip_piechart')
+         )
+         
+         
+),# tabPanel closing 
 
-                  tabPanel("Search by category",
-                           sidebarPanel(
-                             selectInput(inputId = "Day", 
-                                          label = "Choose by day:", 
-                                         choices = c("Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")),
-                             selectInput(inputId = "Borough",
-                                           label = "Choose by borough:",
-                                           choices = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island")),
-                               
-                            selectInput(inputId = "Accessibility",
-                                           label = "Choose by accessibility:",
-                                           choices = c("fully access", "partially access"))
-                               
-                             )
-                           
-                  ), # tabPanel closing 
 
-tabPanel("Search", icon = icon("table"), 
-         tags$style(HTML("
+
+# ------------------------------ tab panel - Interactive Plot By Day - Jingshu ------------------------
+tabPanel("Interactive Plot By Day",
+         
+         
+         fluidPage(
+           # Page title ----
+           titlePanel("Library Availability by Day"),
+           
+           # Sidebar layout with input and output definitions ----
+           sidebarLayout(
+             position = 'left',
+             
+             # Sidebar panel for inputs ----
+             sidebarPanel(
+               
+               # Input: Slider for the number of bins ----
+               selectInput(inputId = "mmm", 
+                           label = "Choose by day:", 
+                           choices = c("Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
+               
+             ), #sidebarpanel closes
+             
+             
+             
+             
+             
+             # Main panel for displaying outputs ----
+             mainPanel(
+               
+               # Output: Histogram ----
+               plotOutput(outputId = "distPlot")
+               
+               
+             ) # MainPanel closes
+             
+           ) # Sidebarlayout closes
+           
+           
+           
+         ) # Fluidpage closes
+         
+         
+         
+), # tabPanel closes
+
+
+
+
+
+
+
+
+
+
+########
+             
+             
+             
+             
+             
+             
+             
+             
+             ######Search ########
+             
+             tabPanel("Search", icon = icon("table"), 
+                      tags$style(HTML("
                     .dataTables_filter input {
                             color: black;
                             background-color: black;
@@ -87,17 +190,65 @@ tabPanel("Search", icon = icon("table"),
                     }
 
                    "
-         )),
-         
-         DT::dataTableOutput("search_result")), 
+                      )),
+                      
+                      DT::dataTableOutput("search_result")), 
+             
+             #https://shiny.rstudio.com/articles/datatables.html
+             
+             
+             tabPanel("Reference",
+                      tags$img(
+                        src = "https://live.staticflickr.com/5151/5914733818_983804b390_b.jpg",
+                        width = "100%",
+                        style = "opacity: 0.90"
+                      ),
+                      fluidRow(
+                        absolutePanel(
+                          style = "background-color: rgba(0, 0, 0, .3)",
+                          top = "40%",
+                          left = "25%",
+                          right = "25%",
+                          height = 270,
+                          tags$p(
+                            style = "padding: 5%; background-color: rgba(0, 0, 0, .3); color: White;font-size: 120%;",
+                            "Data Source:Queens libraries provided by NY OpenData https://data.cityofnewyork.us/Education/Queens-Library-Branches/kh3d-xhq7 "
+                          ),
+                          tags$p(
+                            style = "padding: 5%; background-color: rgba(0, 0, 0, .3); color: White;font-size: 120%;", 
+                            "Acknowledgements: This application is built using the R Shiny App. The following R packages were used: 
+shiny  
+leaflet 
+dplyr 
+magrittr 
+mapview 
+leafsync 
+rsconnect 
+tidyr
+geojson
+tidyverse
+"
+                          ),
+                          tags$p(
+                            style = "padding: 5%; background-color: rgba(0, 0, 0, .3); color: White;font-size: 120%;", 
+                            "Contributors
+Liang Hu lh3057@columbia.edu
+Kejun Liu kl3434@columbia.edu
+Yunfan Liu yl5111@columbia.edu
+Han Wang hw2900@columbia.edu
+Jingshu Zhang jz3552@columbia.edu
+Renqi Zhang rz2600@columbia.edu"
 
-#https://shiny.rstudio.com/articles/datatables.html
-
-
-      tabPanel("Reference",
-                     
-                  ) # tabPanel closing 
-
-  
-    ) #navbarPage closing  
+                          )
+                        )
+                      ),
+                      
+                      
+                      
+                      
+                      
+             ) # tabPanel closing 
+             
+             
+  ) #navbarPage closing  
 ) #Shiny UI closing    
